020a5b1d8f5dbf2c1f30d6d52efae7f138fead78
lilei <lilei@lilei.lan>
2018-07-03 18:46:37 +0800

    主分支开发1

---
+++ EmptyDemo/EmptyDemo/Master1.h
@@ -0,0 +1,13 @@
+//
+//  Master1.h
+//  EmptyDemo
+//
+//  Created by lilei on 2018/7/3.
+//  Copyright © 2018年 lilei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Master1 : NSObject
+
+@end
---
+++ EmptyDemo/EmptyDemo/Master1.m
@@ -0,0 +1,13 @@
+//
+//  Master1.m
+//  EmptyDemo
+//
+//  Created by lilei on 2018/7/3.
+//  Copyright © 2018年 lilei. All rights reserved.
+//主分支开发1
+
+#import "Master1.h"
+
+@implementation Master1
+
+@end
