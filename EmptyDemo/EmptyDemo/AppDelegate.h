e7db497a53e787a85bd884af95f914d1d17a71e8
lilei <lilei@lilei.lan>
2018-07-03 18:19:49 +0800

    第一次初始化项目

---
+++ EmptyDemo/EmptyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmptyDemo
+//
+//  Created by lilei on 2018/7/3.
+//  Copyright © 2018年 lilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
