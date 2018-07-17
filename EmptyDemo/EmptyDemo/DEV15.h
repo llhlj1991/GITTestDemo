eae4f69e83557ebe7cb4f7a787d6d51b3bf1d0f1
llhlj1991 <1608968113@qq.com>
2018-07-17 23:01:00 +0800

    dev15

---
+++ EmptyDemo/EmptyDemo/DEV15.h
@@ -0,0 +1,13 @@
+//
+//  DEV15.h
+//  EmptyDemo
+//
+//  Created by lilei on 2018/7/17.
+//  Copyright © 2018年 lilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEV15 : UIView
+
+@end
---
+++ EmptyDemo/EmptyDemo/DEV15.m
@@ -0,0 +1,21 @@
+//
+//  DEV15.m
+//  EmptyDemo
+//
+//  Created by lilei on 2018/7/17.
+//  Copyright © 2018年 lilei. All rights reserved.
+//
+
+#import "DEV15.h"
+
+@implementation DEV15
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
